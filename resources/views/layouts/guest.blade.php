d4f757af05841b7e36b5defd6df6e727eb0141b3
barber <ebarlotta@yahoo.com.ar>
2023-10-01 09:25:25 -0300

    arreglanglo bootstrap

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,6 +7,9 @@
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
+        <!-- Bootstrap -->
+    <link href="{{ asset('bootstrap/bootstrap.min.css') }}" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
